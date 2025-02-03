baa6bb2fa56269f6be9902f24501ce492991e499
fusodoya <fusodoya@gmail.com>
2025-02-03 22:43:50 +0700

    init project

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB database connection established successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
